d669976275678d9dc2492cb9f68bb969234e94a6
Connor Rhodes <connor@rhodes.contact>
2022-02-01 15:23:59 -0600

    added template code

---
+++ day-2-data-types-and-string-manipulation/exercise-2-BMI-calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇

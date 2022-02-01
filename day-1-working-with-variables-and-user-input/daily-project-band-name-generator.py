13280ba60756a4477a0974d7703bea4df886391a
Connor Rhodes <connor@rhodes.contact>
2022-02-01 13:57:52 -0600

    fixed missing paren

---
+++ day-1-working-with-variables-and-user-input/daily-project-band-name-generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+print("Welcome to the band name generator")
+
+city = input("What is the name of the city you grew up in?\n")
+
+pet = input("What kind of pet did you have as a child?\n")
+
+print("city is " + city)
+
+print("pet is " + pet)

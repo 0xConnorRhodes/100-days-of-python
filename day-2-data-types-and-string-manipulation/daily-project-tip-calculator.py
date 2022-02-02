079265c8fa49b36035c433980e653056ec20d6f4
Connor Rhodes <connor@rhodes.contact>
2022-02-01 20:24:57 -0600

    add debug code and update tip equation

--- day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
+++ day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
@@ -8,6 +8,10 @@ tip_pct = 100 / float(input("What percentage tip would you like to give? "))
 
 split_amt = int(input("How many people will split the bill? "))
 
-individual_amt = bill_amt * tip_pct / split_amt
+individual_amt = bill_amt + (bill_amt * tip_pct) / split_amt
+
+print(f"bill_amt is {bill_amt}")
+print(f"tip_pct is {tip_pct}")
+print(f"split_amt is {split_amt}")
 
 print(f"Each person should pay ${individual_amt}")

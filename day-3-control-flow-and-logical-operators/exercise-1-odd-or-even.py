ff38fa687cfa1c6471be7b0afcc9960eb3227d19
Connor Rhodes <connor@rhodes.contact>
2022-02-02 16:58:37 -0600

    added initial project code

---
+++ day-3-control-flow-and-logical-operators/exercise-1-odd-or-even.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+

d19c83710e504ea269b87865a68b7111186e14d2
Connor Rhodes <connor@rhodes.contact>
2022-02-01 20:28:31 -0600

    break total into two variables for easier debugging

--- day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
+++ day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
@@ -4,14 +4,12 @@ print("Welcome to the Tip Calculator")
 
 bill_amt = float(input("What was the total bill? $"))
 
-tip_pct = 100 / float(input("What percentage tip would you like to give? "))
+tip_pct = float(input("What percentage tip would you like to give? ")) / 100
 
 split_amt = int(input("How many people will split the bill? "))
 
-individual_amt = bill_amt + (bill_amt * tip_pct) / split_amt
+total_amt = bill_amt * tip_pct
 
-print(f"bill_amt is {bill_amt}")
-print(f"tip_pct is {tip_pct}")
-print(f"split_amt is {split_amt}")
+individual_amt = total_amt / split_amt
 
 print(f"Each person should pay ${individual_amt}")

581c1157d4e889a97975f851aade1a9ed9997130
Connor Rhodes <connor@rhodes.contact>
2022-02-01 20:22:20 -0600

    initial attempt, gives wrong number

--- day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
+++ day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
 print("Welcome to the Tip Calculator")
+
+bill_amt = float(input("What was the total bill? $"))
+
+tip_pct = 100 / float(input("What percentage tip would you like to give? "))
+
+split_amt = int(input("How many people will split the bill? "))
+
+individual_amt = bill_amt * tip_pct / split_amt
+
+print(f"Each person should pay ${individual_amt}")

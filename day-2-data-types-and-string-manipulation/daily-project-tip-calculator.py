4207b8d1eb9515c0db0a63778901758f21a9df4f
Connor Rhodes <connor@rhodes.contact>
2022-02-01 21:18:31 -0600

    added format strings to always show answers to two decimal points

--- day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
+++ day-2-data-types-and-string-manipulation/daily-project-tip-calculator.py
@@ -9,10 +9,12 @@ tip_pct = float(input("What percentage tip would you like to give? ")) / 100
 split_amt = int(input("How many people will split the bill? "))
 
 total_amt = round(bill_amt + (bill_amt * tip_pct), 2)
+rounded_total_amt = "{:.2f}".format(bill_amt + (bill_amt * tip_pct), 2)
 
-individual_amt = round(total_amt / split_amt, 2)
+#individual_amt = round(total_amt / split_amt, 2)
+individual_amt = "{:.2f}".format(total_amt / split_amt, 2)
 
-print(f"The total amount is ${total_amt}.")
+print(f"The total amount is ${rounded_total_amt}.")
 
 print(f"Each person should pay ${individual_amt}.")
 

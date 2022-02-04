176df184fffc88642a770a88d0043d83fa79aea3
Connor Rhodes <connor@rhodes.contact>
2022-02-04 10:58:39 -0600

    added sandbox to play with elif

---
+++ day-3-control-flow-and-logical-operators/sandbox-1-elif.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+height = int(input("How tall are you? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    #age = int(input("what is your age? "))
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+

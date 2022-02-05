bb58fa71f2db88a2cb54117597b9f31f88f9c97d
Connor Rhodes <connor@rhodes.contact>
2022-02-05 10:03:10 -0600

    added leap year formula comments

--- day-3-control-flow-and-logical-operators/exercise-3-leap-year.py
+++ day-3-control-flow-and-logical-operators/exercise-3-leap-year.py
@@ -5,3 +5,8 @@ year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+# a leap year is
+# on every year that is evenly divisible by 4
+# except every year that is¡evenly divisible by 100
+# unless the year is also evenly divisible by 400

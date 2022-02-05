f33b8f9d0223aa5ab7544065fe070bc2dba77a6c
Connor Rhodes <connor@rhodes.contact>
2022-02-05 09:19:51 -0600

    added initial project code

---
+++ day-3-control-flow-and-logical-operators/exercise-2-BMI-calc-v2.py
@@ -0,0 +1,8 @@
+#/usr/bin/env python3
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇

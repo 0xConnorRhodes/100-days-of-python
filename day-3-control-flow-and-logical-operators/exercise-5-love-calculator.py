e48c5da534c2986a663a8fd0e77777a04bc9d4eb
Connor Rhodes <connor@rhodes.contact>
2022-02-08 12:27:03 -0800

    added initial project code

---
+++ day-3-control-flow-and-logical-operators/exercise-5-love-calculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# BEGIN project code
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# END project code
+
+

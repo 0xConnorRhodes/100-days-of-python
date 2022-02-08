060eb3998eb7a89a7ab7476a15e88285e55a64d1
Connor Rhodes <connor@rhodes.contact>
2022-02-08 11:12:22 -0800

    added project code

---
+++ day-3-control-flow-and-logical-operators/exercise-4-pizza-order.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# BEGIN project code
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# END project code

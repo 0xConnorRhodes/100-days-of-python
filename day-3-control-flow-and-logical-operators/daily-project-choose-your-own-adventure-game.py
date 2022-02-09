091a8105e765bac8a750255e2216246c1719f785
Connor Rhodes <connor@rhodes.contact>
2022-02-09 10:53:37 -0600

    added initial few decisions

--- day-3-control-flow-and-logical-operators/daily-project-choose-your-own-adventure-game.py
+++ day-3-control-flow-and-logical-operators/daily-project-choose-your-own-adventure-game.py
@@ -3,3 +3,9 @@
 print("Welcome to Treasure Island!")
 print("Your mission is to find the treasure.")
 
+choice1 = input('You\'re at a cross road. Where would you like to go? Type "left" or "right"')
+
+if choice1 == "left"
+    choice2 = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
+    if choice2 == "wait"
+        

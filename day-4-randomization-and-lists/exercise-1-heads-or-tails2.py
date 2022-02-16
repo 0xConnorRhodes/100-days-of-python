c7c4bbe87786e8686dd7b38aaf5711e6dc6f4554
Connor Rhodes <connor@rhodes.contact>
2022-02-16 12:36:27 -0600

    added initial project code

---
+++ day-4-randomization-and-lists/exercise-1-heads-or-tails2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import random
+
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write the rest of your code below this line 👇

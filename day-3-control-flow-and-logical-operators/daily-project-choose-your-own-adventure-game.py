4bf32844988eae3f32bcb090d08272185e0e607c
Connor Rhodes <connor@rhodes.contact>
2022-02-15 21:13:11 -0600

    added initial choice

--- day-3-control-flow-and-logical-operators/daily-project-choose-your-own-adventure-game.py
+++ day-3-control-flow-and-logical-operators/daily-project-choose-your-own-adventure-game.py
@@ -27,7 +27,9 @@ print("Your mission is to find the treasure.")
 
 choice1 = input('You\'re at a cross road. Where would you like to go? Type "left" or "right"')
 
-if choice1 == "left"
+
+if choice1 == "right":
+    print("You have died of dysentery.")
+    exit()
+else:
     choice2 = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
-    if choice2 == "wait"
-        

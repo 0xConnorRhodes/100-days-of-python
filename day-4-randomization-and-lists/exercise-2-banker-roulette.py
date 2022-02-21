47b06947b00ed596a21ee39287ce339a8ba3db26
Connor Rhodes <connor@rhodes.contact>
2022-02-20 21:44:32 -0600

    finished challenge

--- day-4-randomization-and-lists/exercise-2-banker-roulette.py
+++ day-4-randomization-and-lists/exercise-2-banker-roulette.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import random
+
 # 🚨 Don't change the code below 👇
 test_seed = int(input("Create a seed number: "))
 random.seed(test_seed)
@@ -10,4 +12,10 @@ names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+number_of_people = len(names)
+
+winner = names[random.randint(0, number_of_people - 1)]
+
+print(f"{winner} is going to buy the meal today!")
 

696827112eae771cc2fa3aefb35a161891053c58
Connor Rhodes <connor@rhodes.contact>
2022-02-21 14:56:36 -0600

    completed script

--- day-4-randomization-and-lists/daily-project-rock-paper-scissors.py
+++ day-4-randomization-and-lists/daily-project-rock-paper-scissors.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-rock_ascii = '''
+import random
+
+rock = '''
     _______
 ---'   ____)
       (_____)
@@ -9,7 +11,7 @@ rock_ascii = '''
 ---.__(___)
 '''
 
-paper_ascii = '''
+paper = '''
     _______
 ---'   ____)____
           ______)
@@ -18,7 +20,7 @@ paper_ascii = '''
 ---.__________)
 '''
 
-scissors_ascii = '''
+scissors = '''
     _______
 ---'   ____)____
           ______)
@@ -27,3 +29,31 @@ scissors_ascii = '''
 ---.__(___)
 '''
 
+human_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+computer_choice = random.randint(0,2)
+
+options = [rock, paper, scissors]
+
+print(options[human_choice])
+
+print(f"Computer chose:\n{options[computer_choice]}")
+
+if human_choice == 0 and computer_choice == 0:
+    print("It's a tie.")
+elif human_choice == 1 and computer_choice == 0:
+    print("You win!")
+elif human_choice == 2 and computer_choice == 0:
+    print("You lose.")
+elif human_choice == 0 and computer_choice == 1:
+    print("You lose.")
+elif human_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif human_choice == 1 and computer_choice == 1:
+    print("It's a tie.")
+elif human_choice == 1 and computer_choice == 2:
+    print("You lose.")
+elif human_choice == 2 and computer_choice == 1:
+    print("You win!")
+elif human_choice == 2 and computer_choice == 2:
+    print("It's a tie.")

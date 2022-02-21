3591aac007a1771b8ef40f91d349f19f110f3187
Connor Rhodes <connor@rhodes.contact>
2022-02-20 21:32:03 -0600

    added initial project code

---
+++ day-4-randomization-and-lists/exercise-2-banker-roulette.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+

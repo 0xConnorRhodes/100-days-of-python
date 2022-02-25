133ea67616327e642f91b3b00a75928b7928734a
Connor Rhodes <connor@rhodes.contact>
2022-02-25 10:55:51 -0500

    updated script to use for-loop instead of the len() function

--- day-5-for-loops/exercise-1-average-height.py
+++ day-5-for-loops/exercise-1-average-height.py
@@ -11,10 +11,14 @@ for n in range(0, len(student_heights)):
 
 total_height = 0
 
+# calculate total height
 for height in student_heights:
     total_height += height
 
-number_of_students = len(student_heights)
+# calculate number of students
+number_of_students = 0
+for item in student_heights:
+    number_of_students += 1
 
 avg_height = total_height / number_of_students
 
